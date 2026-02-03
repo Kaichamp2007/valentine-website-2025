da2319d8ad039af8e4f77a156524cc7d31f98b8a
Kaichamp2007 <142719376+Kaichamp2007@users.noreply.github.com>
2026-02-03 22:56:12 +0200

    Update config.js

--- config.js
+++ config.js
@@ -30,10 +30,10 @@ const CONFIG = {
         second: {
             text: "How much do you love me?",                          // For the love meter
             startText: "This much!",                                   // Text before the percentage
-            nextBtn: "Next ❤️"                                         // Text for the next button
+            nextBtn: "Next"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine on February 14th, 2025? 🌹", // The big question!
+            text: "Will you be my Valentine on February 14th, 2026? 🌹", // The big question!
             yesBtn: "Yes!",                                             // Text for "Yes" button
             noBtn: "No"                                                 // Text for "No" button
         }
